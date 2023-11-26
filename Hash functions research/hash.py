88720184e847585f2664aa08e9f9ade23a5617fb
finstape <finstape@mail.ru>
2023-11-26 13:22:42 -0500

    Complete code

---
+++ Hash functions research/hash.py
@@ -0,0 +1,34 @@
+﻿import hashlib
+
+TEST_SALTS = [1, 1000, 100000, 291673, 9999999, 30000000, 123456789, 500000000, 999999999, 100000000] 
+
+def hash_md5(data):
+    h = hashlib.md5(str(data).encode('utf-8'))
+    return h.hexdigest()
+
+def hash_sha1(data):
+    h = hashlib.sha1(str(data).encode('utf-8'))
+    return h.hexdigest()
+
+def hash_sha256(data):
+    h = hashlib.sha256(str(data).encode('utf-8'))
+    return h.hexdigest()
+
+def write_to_file(file_path, data):
+    with open(file_path, "w") as f:
+        for item in data:
+            f.write(str(item) + '\n')
+
+with open("numbers.txt", "r") as f:
+    NUMBERS = [int(line) for line in f.readlines()]
+
+for salt in TEST_SALTS:
+    salted_numbers = [num + salt for num in NUMBERS]
+    
+    MD5_HASHES = [hash_md5(num) for num in salted_numbers]
+    SHA1_HASHES = [hash_sha1(num) for num in salted_numbers]
+    SHA256_HASHES = [hash_sha256(num) for num in salted_numbers]
+
+    write_to_file(f"hashed/md5_{str(salt)}.txt", MD5_HASHES)
+    write_to_file(f"hashed/sha1_{str(salt)}.txt", SHA1_HASHES)
+    write_to_file(f"hashed/sha256_{str(salt)}.txt", SHA256_HASHES)
---
+++ Hash functions research/main.cpp
@@ -0,0 +1,55 @@
+﻿#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void readNumbersFromFile(const string& fileName, vector<ll>& numbers, ll& mx_num) {
+    ifstream file(fileName);
+    string tt, str;
+    ll tmp;
+
+    while (getline(file, tt)) {
+        stringstream sst(tt);
+        getline(sst, str, ':');
+        getline(sst, str, '\n');
+        stringstream ss(str);
+        ss >> tmp;
+        mx_num = max(mx_num, tmp);
+        numbers.push_back(tmp);
+    }
+
+    file.close();
+}
+
+int main(){
+	system("hashcat -m 0 -a 3 hashed_numbers.txt ?d?d?d?d?d?d?d?d?d?d?d -o unhashed_numbers.txt --show");
+    	vector<ll> numbers;
+	vector<ll> dict = {89156617519, 89866878664, 89636187893, 89038679411, 89868468991};
+    	string str, tt;
+    	ll tmp, mx_num = 0;
+    	readNumbersFromFile("unhashed_numbers.txt", numbers, mx_num);
+    	vector<ll> salts;
+    	for (const auto &x : numbers) {
+		for (const auto &y : dict) salts.push_back(y-x);
+    	}
+    	ll ans_salt;
+    	for (const auto &salt : salts) {
+		vector<bool> k(dict.size(), false);
+		for (const auto &x : numbers) {
+	    		for (ll i = 0; i < dict.size(); ++i) {
+				if (x+salt == dict[i]) k[i] = true;
+	    		}
+		}
+		ll sm = 0;
+		for (const auto &x : k) sm += x;
+		if (sm == dict.size()) {
+	    		cout << "Salt = " << -salt << endl;
+	    		ans_salt = salt;
+	    		break;
+		}
+    	}
+    	ofstream out("numbers.txt");
+    	for (auto &element : numbers) {
+		out << element + ans_salt << endl;
+    	}
+    	out.close();
+}
